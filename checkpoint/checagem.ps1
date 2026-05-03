404e45f39d6b8acccbbe5e260312e87d63042981
Gui <guilhermeodatruf@outlook.com>
2026-05-03 19:12:56 -0300

    Aplicando estruturas condicionais

--- checkpoint/checagem.ps1
+++ checkpoint/checagem.ps1
@@ -1,3 +1,21 @@
 # DigBat
 
-echo "Hello World"
+# variáveis de acesso
+$basePath = "$env:USERPROFILE\battery_report"
+$logPath = "$basePath\logs.txt"
+$dataPath = "$basePath\battery-data.csv"
+
+# Caso diretório não exista
+if (!(Test-Path $basePath)) {
+  New-Item -ItemType Directory -Path $basePath | Out-Null
+}
+
+# Pergunta ao usuário
+$verDiagnostico = Read-Host "Deseja rodar o diagnóstico completo da bateria (s/n)? "
+
+if ($verDiagnostico -notin @("s", "S", "sim", "Sim")) {
+  Write-Host "Operação cancelada."
+  exit
+}
+
+# Momento da execução
